d390f668af113a21a8407a88d8d97499cbc7022b
BikkIlshat <HeavyIlshat@rambler.ru>
2022-02-01 19:47:54 +0300

    описаны adapterDelegates

---
+++ app/src/main/java/com/github/bikkIlshat/mdbforgames/base/ListItem.kt
@@ -0,0 +1,6 @@
+package com.github.bikkIlshat.mdbforgames.base
+
+/***
+ * Базовый interface (любой элемент списка который отправляется в recyclerView в adapter  он должен быть наследником ListItem
+ */
+interface ListItem
